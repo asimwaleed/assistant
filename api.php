a49e012c4d1517b6b59b2ded7c7084d6202047df
Waleed <waleedasim208@gmail.com>
2019-08-27 12:49:44 +0200

    dc

--- api.php
+++ api.php
@@ -20,7 +20,7 @@ function processMessage($update){
 function sendMessage($parameters){
 	$req_dump = print_r($parameters, true);
 	$fp = file_put_contents('request.log', $req_dump);
-	header('Content-Type; application/json');
+	header('Content-Type: application/json');
 	echo json_encode($parameters);
 }
 
