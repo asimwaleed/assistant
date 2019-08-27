f9236b54e78a1fc76f09112e2e1053c4c116bf7e
Waleed <waleedasim208@gmail.com>
2019-08-27 13:50:57 +0200

    sadasd

--- api.php
+++ api.php
@@ -1,32 +1,7 @@
-
 <?php
 
-
-
 header('Content-Type: application/json');
-function processMessage($update){
-	if ($update["result"]["action"] == "") {
-
-		$fp = file_put_contents('request.log', $update["result"]["parameters"]["msg"]);
-		sendMessage(array(
-"source" => $update["result"]["source"],
-"speech" => $update["result"]["parameters"]["msg"],
-"displayText" => "...........Text Here........",
-"contextOut" => array()
-		));
-		# code...
-	}
-}
-function sendMessage($parameters){
-	$req_dump = print_r($parameters, true);
-	$fp = file_put_contents('request.log', $req_dump);
-	header('Content-Type: application/json');
-	echo json_encode($parameters);
-}
-
-$update_response = file_get_contents("php://input");
-$update =json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
-	processMessage($update);
-}
+$request = file_get_contents('php://input');
+$req_dump = print_r( $request, true );
+$fp = file_put_contents( 'request.log', $req_dump );
 ?>
