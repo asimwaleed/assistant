231f9e6641cb341b18847b96036a57666d9618b9
Waleed <waleedasim208@gmail.com>
2019-08-27 12:36:41 +0200

    dd

--- api.php
+++ api.php
@@ -26,6 +26,7 @@ function sendMessage($parameters){
 
 $update_response = file_get_contents("php://input");
 $update =json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
 	processMessage($update);
-
+}
 ?>
